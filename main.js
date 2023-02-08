0c3dab6cc453592ae64dc9e1a9b98a4a09165b1f
Gerardo Mundo <ger.mundo66@gmail.com>
2023-02-07 20:39:23 -0600

    Añadida función para guardar en el localStorage

--- main.js
+++ main.js
@@ -5,7 +5,12 @@ const app = new Vue({
     tasks: [],
     newTask: "",
   },
+
   methods: {
+    saveItem: function () {
+      localStorage.setItem("vue-item", JSON.stringify(this.tasks));
+    },
+
     addTask: function () {
       this.tasks.push({
         name: this.newTask,
@@ -13,12 +18,26 @@ const app = new Vue({
       });
 
       this.newTask = "";
+      this.saveItem();
     },
+
     changeState: function (index) {
       this.tasks[index].state = true;
+      this.saveItem();
     },
+
     deleteTask: function (index) {
       this.tasks.splice(index, 1);
+      this.saveItem();
     },
+  },
+  created: function () {
+    let data = JSON.parse(localStorage.getItem("vue-item"));
+
+    if (data === null) {
+      this.tasks = [];
+    } else {
+      this.tasks = data;
+    }
   },
 });
